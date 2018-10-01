ae8d3adc57999e98c7b5a9f62d01714cf6ec27d3
Nathalie Santana <nathalie.scruz@gmail.com>
2018-10-01 04:10:02 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   
    def change
-    change_column :students, :grade, :integer
+    change_column :students, :birthdate, :datetime
     
     end
   end
