e3092691fb988f8d080cd57a50a7fd74dcb350fe
Nathalie Santana <nathalie.scruz@gmail.com>
2018-10-01 04:05:59 +0000

    Done.

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   
    def change
-    create_table :students do |t|
-      t.string :name
+    add_column :students, :grade, :integer
+    add_column :student, :birthdate, :string
     end
   end
   
--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   
    def change
-    create_table :students do |t|
-      t.string :name
+    change_column :students, :grade, :integer
+    
     end
   end
   
